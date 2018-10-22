d88e257902e200681a0855bf089cbed852ab5e9f
fadikamohamed <fadika.mohamed02@gmail.com>
2018-10-22 09:30:40 +0200

    Exercice terminé

---
+++ charactere.php
@@ -0,0 +1,25 @@
+<?php
+
+require_once 'orc.php';
+
+class Charactere {
+
+    private $health;
+    private $mana;
+    private $charac;
+
+    public function construct() {
+        
+    }
+
+    public function destruct() {
+        
+    }
+
+    public function chat($charac) {
+        
+    }
+
+}
+
+?>
---
+++ orc.php
@@ -0,0 +1,19 @@
+<?php
+
+require_once 'charactere.php';
+
+class Orc {
+
+    private $health = 1000;
+
+    public function useHAMMER() {
+        
+    }
+
+    public function useSword() {
+        
+    }
+
+}
+
+?>
